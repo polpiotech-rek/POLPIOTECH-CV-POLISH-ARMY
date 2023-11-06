737c0e5894562dcfc7719684ca1c97d046ce928b
Piotr Bodych <142532356+polpiotech-rek@users.noreply.github.com>
2023-11-06 11:49:34 +0100

    Add files via upload

---
+++ Curriculum Vitae/script.js
@@ -0,0 +1,9 @@
+const yourName = prompt("Wpisz swoje imię:");
+const nameCompany = prompt("Podaj nazwę firmy:")
+
+if (yourName, nameCompany) {
+    alert("Witaj " + yourName + " na stronie z moim życiorysem zawodowym! \nMam nadzieję, że moje kompetencje spełnią wymagania potrzebnę do objęcia stanowiska w firmię " + nameCompany + ". \n \nZ poważaniem, \nPiotr Kazimierz Bodych");
+}
+/*else {
+    alert("")
+}*/
